8631a9cb59135fcb1ac43a7731b848cb60d81b49
timHa91 <tim.andre.hartmann@gmail.com>
2024-03-16 23:48:51 +0100

    seeded the db

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -3,6 +3,8 @@ import { RealEstateItem } from "../shared/real-estate-item.model";
 import { Observable, Subject, catchError, of, tap } from "rxjs";
 import { DataService } from "../data.service";
 import { MapboxService } from "../mapbox/mapbox.service";
+import { Category } from "../shared/category.enum";
+import { GeoJson } from "../shared/geo.model";
 
 @Injectable({providedIn: 'root'})
 export class SearchService {
@@ -13,48 +15,48 @@ export class SearchService {
 
     constructor(private dataService: DataService, private mapboxService: MapboxService) {}
 
-    // searchResults: RealEstateItem[] = [
-    //     {
-    //         address: '1121 48th Place Northeast Washington, DC',
-    //         description: 'New Construction - 4 BD 3 BA',
-    //         image: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D',
-    //         price: 2500,
-    //         category: Category.Rent,
-    //         geometry: new GeoJson([-76.932458, 38.905337])
-    //     },
-    //     {
-    //         address: '5324 James Place Northeast Washington, DC',
-    //         description: '3 BD 2 BA Single Family Home',
-    //         image: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D',
-    //         price: 228800,
-    //         category: Category.Sold,
-    //         geometry: new GeoJson([-76.92377, 38.90098])
-    //     },
-    //     {
-    //         address: '3438 Croffut Place Southeast',
-    //         description: '4 BD 2 BD Townhome',
-    //         image: 'https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8fA%3D%3D',
-    //         price: 299900,
-    //         category: Category.Sale,
-    //         geometry: new GeoJson([-76.95642, 38.88696])
-    //     },
-    //     {
-    //         address: '539 59th Street Northeast Washington, DC',
-    //         description: '3 BD 2BA SIngle Family Home',
-    //         image: 'https://images.unsplash.com/photo-1572120360610-d971b9d7767c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fHw%3D',
-    //         price: 300000,
-    //         category: Category.Sale,
-    //         geometry: new GeoJson([-76.916156, 38.896954])
-    //     },
-    //     {
-    //         address: '527 45th Street Northeast Washington, DC',
-    //         description: 'Townhome - 4 BD 3 BA',
-    //         image: 'https://images.unsplash.com/photo-1625602812206-5ec545ca1231?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW1lcmljYW4lMjBob3VzZXN8ZW58MHx8MHx8fDA%3D',
-    //         price: 125000,
-    //         category: Category.Sale,
-    //         geometry: new GeoJson([-76.9380523, 38.892946])
-    //     }
-    // ]
+    seedList: RealEstateItem[] = [
+        {
+            address: '1121 48th Place Northeast Washington, DC',
+            description: 'New Construction - 4 BD 3 BA',
+            image: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D',
+            price: 2500,
+            category: Category.Rent,
+            geometry: new GeoJson([-76.932458, 38.905337])
+        },
+        {
+            address: '5324 James Place Northeast Washington, DC',
+            description: '3 BD 2 BA Single Family Home',
+            image: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D',
+            price: 228800,
+            category: Category.Sold,
+            geometry: new GeoJson([-76.92377, 38.90098])
+        },
+        {
+            address: '3438 Croffut Place Southeast',
+            description: '4 BD 2 BD Townhome',
+            image: 'https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8fA%3D%3D',
+            price: 299900,
+            category: Category.Sale,
+            geometry: new GeoJson([-76.95642, 38.88696])
+        },
+        {
+            address: '539 59th Street Northeast Washington, DC',
+            description: '3 BD 2BA SIngle Family Home',
+            image: 'https://images.unsplash.com/photo-1572120360610-d971b9d7767c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fHw%3D',
+            price: 300000,
+            category: Category.Sale,
+            geometry: new GeoJson([-76.916156, 38.896954])
+        },
+        {
+            address: '527 45th Street Northeast Washington, DC',
+            description: 'Townhome - 4 BD 3 BA',
+            image: 'https://images.unsplash.com/photo-1625602812206-5ec545ca1231?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW1lcmljYW4lMjBob3VzZXN8ZW58MHx8MHx8fDA%3D',
+            price: 125000,
+            category: Category.Sale,
+            geometry: new GeoJson([-76.9380523, 38.892946])
+        }
+    ]
 
     loadData(): Observable<RealEstateItem[]> {
         this.onFetching$.next(true);
